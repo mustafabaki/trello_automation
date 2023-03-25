e5808be9551b4c321628807d3229090b2fdfdce7
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2023-03-25 20:54:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 from trello import * 
+from github import * 
 
-doing_id = get_trello_list_id("Doing")
+""" doing_id = get_trello_list_id("Doing")
 
 card_id = get_card_id_by_name("find api")
 
-list_id = get_trello_list_id("Doing")
+list_id = get_trello_list_id("Doing") """
 
+
+for item in load_commits():
+    print(item)
