04a1c3126615f82bd7780ef75b6d128c8a0d2eb1
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2023-03-26 15:45:48 +0300

    done: test the project

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ for item in load_commits():
     elif "done" in item.lower():
         # move the card to doing list ...
         list_id = get_trello_list_id("Done")
-        commit_message = item.split()
-        card_id = get_card_id_by_name(commit_message)
+        commit_message = item.split(":")
+        message = commit_message[1].strip()
+        if ':' not in item:
+            raise ValueError("the commit message is not a valid commit message")
+        card_id = get_card_id_by_name(message)
         move_trello_card(card_id=card_id, list_id=list_id)
 
 
