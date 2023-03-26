ce653034dc6a3da609cace6a6ab7774a7cf4f5b9
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2023-03-26 15:35:20 +0300

    test 2

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ list_id = get_trello_list_id("Doing") """
 
 
 for item in load_commits():
+    
     if "doing" in item.lower():
         # move the card to doing list ...
         list_id = get_trello_list_id("Doing")
-        commit_message = item.split()
-        card_id = get_card_id_by_name(commit_message)
+        commit_message = item.split(":")
+        if len(commit_message) == 1:
+            message = commit_message[0]
+        else:
+            message = commit_message[1].trim()
+        card_id = get_card_id_by_name(message)
         move_trello_card(card_id=card_id, list_id=list_id)
     
     elif "done" in item.lower():
